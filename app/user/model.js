3aeec66e120c67cb3eb78a66f8af24d19124df05
martypenner <penner.marty@gmail.com>
2015-04-23 19:31:19 -0400

    Show form errors with some basic animation

--- app/user/model.js
+++ app/user/model.js
@@ -13,13 +13,21 @@ export default DS.Model.extend(EmberValidations.Mixin, {
 
     validations: {
         name: {
-            presence: {message: 'abc'}
+            presence: {message: 'You must provide your name.'}
         },
         email: {
-            format: {'with': /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i}
+            format: {
+                'with': /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i,
+                allowBlank: false,
+                message: 'Your email should look something like this: "name@example.com".'
+            }
         },
         postalCode: {
-            format: {'with': /[a-z]\d[a-z] ?\d[a-z]\d/i, allowBlank: false}
+            format: {
+                'with': /^[a-z]\d[a-z] ?\d[a-z]\d$/i,
+                allowBlank: false,
+                message: 'Your postal code should look something like this: "A1A 2B2"'
+            }
         }
     }
 });
